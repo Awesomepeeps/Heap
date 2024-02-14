db71bca4f7fe9225d7f3e18b961bb49abdac999e
RiseSky <67874744+Awesomepeeps@users.noreply.github.com>
2024-02-14 13:48:46 -0800

    finished

--- heap.cpp
+++ heap.cpp
@@ -13,32 +13,98 @@ private:
 
 public:
     void heapup(int index) {
+        if (index == 1) {
+            return;
+        }
+        int parent = floor(index / 2);
+        if (heap[index] > heap[parent]) {
+            swap(heap[index], heap[parent]);
+            heapup(parent);
+        }
     }
 
     void heapdown(int index) {
+        int leftchild = 2 * index;
+        int rightchild = 2 * index + 1;
+
+        if (leftchild > size) {
+            return;
+        }
+
+        int largest = index;
+        if (heap[leftchild] > heap[largest]) {
+            largest = leftchild;
+        }
+        if (rightchild <= size && heap[rightchild] > heap[largest]) {
+            largest = rightchild;
+        }
+
+        if (largest != index) {
+            swap(heap[index], heap[largest]);
+            heapdown(largest);
+        }
     }
 
     maxheap() : size(0) {}
 
     void additem(int value) {
+        if (size >= MAXSIZE) {
+            cout << "Heap is full" << endl;
+            return;
+        }
+        size++;
+        heap[size] = value;
+        heapup(size);
     }
 
     void removeroot() {
+        if (size == 0) {
+            cout << "Heap is empty" << endl;
+            return;
+        }
+        heap[1] = heap[size];
+        size--;
+        heapdown(1);
     }
 
     void removeall() {
+        while (size != 0) {
+            removeroot();
+        }
     }
 
     void printall(int current = 1, int depth = 0) {
+        if (current > size) {
+            return;
+        }
+        printall(2 * current + 1, depth + 1); 
+        for (int i = 0; i < depth; i++) {
+            cout << "\t";
+        }
+        cout << heap[current] << endl;
+        printall(2 * current, depth + 1); 
     }
 
     int getsize() {
+        return size;
     }
 
     int getheap(int index) {
+        return heap[index];
     }
 
-    void addFromFile(string filename) {}
+    void addFromFile(string filename) {
+        ifstream file(filename);
+        if (!file) {
+            cout << "Could not open file" << endl;
+            return;
+        }
+        int value;
+        while (file >> value && size < MAXSIZE) { 
+            additem(value);
+        }
+        file.close(); 
+    }
 };
 
 int main() {
